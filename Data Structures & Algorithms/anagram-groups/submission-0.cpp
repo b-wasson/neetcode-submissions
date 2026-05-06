b0a660c849e87e7446401cf75ede99cd7e3fe2a5
davis wasson <57428308+b-wasson@users.noreply.github.com>
2026-05-06 18:35:44 -0500

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> seen;
+        vector<vector<string>> output;
+        for(const auto& s: strs){
+            string sortedS = s;
+            sort(sortedS.begin(), sortedS.end());
+            seen[sortedS].push_back(s);
+        }
+        vector<vector<string>> result;
+        for(auto& pair : seen){
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
