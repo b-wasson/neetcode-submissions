f6f0d65ac64c980f5358ecfb885765b444837831
davis wasson <57428308+b-wasson@users.noreply.github.com>
2026-05-10 14:53:30 -0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+       int right = s.length() - 1;
+       
+       for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) != s.charAt(right) && i < right){
+                return false;
+            }
+            right--;
+       }
+       
+       return true;
+    }
+}
