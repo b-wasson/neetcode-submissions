66407ae787272419d1ad533ae50a53202bd87a9d
davis wasson <57428308+b-wasson@users.noreply.github.com>
2026-06-03 12:38:12 -0500

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+        int pastNum = Integer.MIN_VALUE;
+        int counter = 1;
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int currLargestLength = 1;
+        List<Integer> currRun = new ArrayList<>();
+
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return 1;
+
+        Arrays.sort(nums);
+
+        for(int num : nums){
+            if(pastNum == num) continue;
+            if(pastNum + 1 == num){
+                counter++;
+
+                if(counter > currLargestLength) currLargestLength = counter; 
+
+                currRun.add(num);
+                pastNum = num;
+                continue;
+            }
+            map.put((Integer) counter, currRun);
+            pastNum = num;
+            counter = 1; 
+        }
+
+
+        return currLargestLength;
+    }
+}
